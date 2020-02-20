ca693b19d698f8c6094c1bcab55b53f125fcaee0
Alexander May <alexander.may@kratzer-automation.com>
2020-02-20 14:43:20 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./tailwind.css";
+
+const App = () => {
+  return (
+    <div className="v-screen h-screen flex justify-center items-center">
+      Hello!
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+module.exports = {
+  theme: {
+    extend: {}
+  },
+  variants: {
+    backgroundColor: ["hover", "focus", "active"]
+  },
+  plugins: []
+};
